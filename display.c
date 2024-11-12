ad537328545ff5365fdd5b98dd2dcee51b561d0c
tangaree <tangaree99@naver.com>
2024-11-12 19:26:33 +0900

    콘솔 색상,초기 맵 설정

--- display.c
+++ display.c
@@ -6,3 +6,15 @@ void setCursorposition(int x, int y) {
 	COORD coord = { x,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+
+void setColor(int color) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void initialize_map(char map[MAP_HEIGHT][MAP_WIDTH]) {
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		for (int j = 0; j < MAP_WIDTH; j++) {
+			map[i][j] = ' ';
+		}
+	}
+}
