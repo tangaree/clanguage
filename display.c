091db44fa05d06f12253a3fa4e11f9299524ccc7
tangaree <tangaree99@naver.com>
2024-11-12 13:20:23 +0900

    커서 위치 설정 함수

--- display.c
+++ display.c
@@ -1,3 +1,9 @@
 #include <stdio.h>
 #include <Windows.h>
 #include "display.h"
+ 
+void setCursorposition(int x, int y) {
+	COORD coord = { x,y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
